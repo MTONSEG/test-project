86146b35ae23f290892d41fc649f1bbfbfa4e5da
Maksym Puzanov <puzanov.maksym@gmail.com>
2024-03-08 14:35:09 +0200

    remove theme provider

--- src/components/containers/Layout/Layout.tsx
+++ src/components/containers/Layout/Layout.tsx
@@ -1,8 +1,6 @@
 import { FC, ReactNode } from 'react'
 import './Layout.scss'
 import Header from 'components/widgets/Header/Header'
-import { ThemeProvider } from '@mui/material'
-import { theme } from 'themes'
 
 interface PropsType {
 	children: ReactNode
@@ -10,13 +8,11 @@ interface PropsType {
 
 const Layout: FC<PropsType> = ({ children }) => {
 	return (
-		<ThemeProvider theme={theme}>
 			<div className='wrapper'>
 				<Header />
 
 				<main className='main'>{children}</main>
 			</div>
-		</ThemeProvider>
 	)
 }
 
